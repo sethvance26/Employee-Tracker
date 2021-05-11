93d9fc875f4d78b2d698c4edd3351bf1d91bacee
Seth <sethvance26@gmail.com>
2021-05-11 17:07:25 -0400

    adding database w sql file

---
+++ employeeTracker.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE Emp_role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL, 
+    department_id INT NOT NULL,
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+);  
+
